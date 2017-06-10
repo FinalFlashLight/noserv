4e1b7401599c9ce9a8d0651c7d27f012c8e53cba
FinalFlashLight <ryan.godwin524@outlook.com>
2017-06-09 23:12:14 -0500

    info bar done

--- js/controllers/championCtrl.js
+++ js/controllers/championCtrl.js
@@ -7,8 +7,10 @@ angular.module('league')
     'background-color': 'red',
     'border-radius': '25px',
   }
+
   var promise = champsService.getChamp(parseInt($stateParams.id)); //for test, should get a champ
   promise.then(function(result){
+    //start of .then
     $scope.champInfo = result.data;
     var numSkins = $scope.champInfo.skins.length;
     $scope.random = Math.floor(Math.random() * numSkins)
@@ -20,6 +22,36 @@ angular.module('league')
       'height': '500px',
       'max-width': '1211px',
       'background-image': "url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + $scope.champInfo.key + "_" + $scope.random + ".jpg')"
+    }
+    $scope.attackbar = {
+      'background-color':'red',
+      'width': $scope.champInfo.info.attack * 20 + 'px',
+      'height': '25px',
+      'border-radius': '5px',
+      'border': '1px solid gold',
+    }
+    $scope.magicbar = {
+      'background-color':'purple',
+      'width': $scope.champInfo.info.magic * 20 + 'px',
+      'height': '25px',
+      'border-radius': '5px',
+      'border': '1px solid gold',
+    }
+    $scope.defensebar = {
+      'background-color':'yellow',
+      'width': $scope.champInfo.info.defense * 20 + 'px',
+      'height': '25px',
+      'border-radius': '5px',
+      'border': '1px solid gold',
+
+    }
+    $scope.difficultybar = {
+      'background-color':'orange',
+      'border-radius': '5px',
+      'border': '1px solid gold',
+      'width': $scope.champInfo.info.difficulty * 20 + 'px',
+      'height': '25px',
+
     }
     $scope.skinName = $scope.champInfo.skins[$scope.random].name;
     console.log($scope.skinName);
--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,3 +1,4 @@
 angular.module('league').controller('homeCtrl', function($scope, homeSrv){
   $scope.test = 'Home page :D';
+  $scope.title = 'Champ Lister Pro';
 });
